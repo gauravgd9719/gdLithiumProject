bd32612b9b9076ded47a917e84ea76ba2f57f11b
gauravgd9719 <gd220310@gmail.com>
2022-10-14 09:23:47 +0530

    add 2 router.get

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,4 +5,13 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/clickme', function (req, res) {
+    res.send('this is calling from gaurav dhiman ')
+});
+
+router.get('/click', function (req, res) {
+    res.send('hi buddy i am gd  ')
+});
+
+
 module.exports = router;

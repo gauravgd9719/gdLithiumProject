ce06032dc956dab6d756815b6c6204f947d7a00d
gauravgd9719 <gd220310@gmail.com>
2022-10-20 17:50:59 +0530

    problem sovle

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+
+// assignment solution
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+  
+
+ router.post('/person-votings', function(req,res){
+    let votingAge = req.query.votingAge
+    let elegibleAge  = persons.filter(x=>x.age>votingAge)
+    res.send({data:elegibleAge, status:true})
+
+    })
+ 
+
+
+
+
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
     console.log(studentName)
